ee4da4552940cb8700a93423797d89c6dc7094f2
Dylan Mahaffey <dylan.mahaffey@gmail.com>
2017-02-19 15:55:55 -0800

    contact validation complete

--- components/navigation/nav.controller.js
+++ components/navigation/nav.controller.js
@@ -12,7 +12,7 @@
             if(this.current == '/resume'){ this.resume='border'}
             else if(this.current == '/contact'){ this.contact='border-white'; this.nav = 'white'}
             else if(this.current == '/portfolio'){ this.portfolio='border-white'; this.nav = 'white'}
-            else{this.else='border'};
+            else{this.else='border-white'; this.nav = 'white'};
 
 
         });
--- partials/controllers/contact.controller.js
+++ partials/controllers/contact.controller.js
@@ -3,9 +3,43 @@
 (function(angular) {
     angular
         .module("application")
-        .controller("contactController", ['contactService', function(service){
-                this.hey = 'dood suh'
+        .controller("contactController", ['$http', function($http){
 
-                service.text()
+
+
+                this.sendEmail = function(name, email, phone, message){
+                    console.log(name + email + phone + message);
+                    if(!name||!email||!message){
+                        this.invalid = true;
+                    }else{
+                        this.thanks = true;
+                $http({
+                    url: "http://formspree.io/dylan.mahaffey@gmail.com",
+                    data: $.param({
+                        name: name,
+                        email: email,
+                        phone, phone,
+                        message: message
+                    }),
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                });
+            }
+            };
+
+            this.close = () => {
+					// hide modal immediately
+					this.thanks = false;
+
+					// clear these thereafter
+					this.form.controls = { };
+					this.form.$setPristine();
+					this.error = null;
+				};
+
+
         }]);
 }(window.angular));
